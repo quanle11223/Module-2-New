648b8b88c1b56be0da068d29c13965bd5f8ab169
tuấn hữu <laquan1311@gmail.com>
2022-10-18 16:51:20 +0700

    ThucHanhbai10

---
+++ src/Bai10/MyList.java
@@ -0,0 +1,31 @@
+package Bai10;
+
+import java.util.Arrays;
+
+public class MyList<E> {
+    private int size = 0;
+    private static int DEFAULT_CAPACITY = 10;
+    private Object elements[] ;
+
+
+    public MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+    private void ensureCaPa(){
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+    public void add(E e){
+        if (size == elements.length){
+            ensureCaPa();
+        }
+        elements[size++] = e;
+    }
+    public E get(int i){
+        if (i > size || i < 0){
+            throw new IndexOutOfBoundsException("index " + i + ", size " + i );
+        }
+        return (E) elements[i];
+    }
+
+}
