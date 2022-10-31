b6429235ab067f7a37d6823bfefbdba2b9272e77
tuấn hữu <laquan1311@gmail.com>
2022-10-31 14:37:54 +0700

    Thuchanh+BaiTap19

---
+++ src/Bai19/BaiTap/ValidateDanTri.java
@@ -0,0 +1,43 @@
+package Bai19.BaiTap;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidateDanTri {
+      try
+    {
+        URL url = new URL("https://dantri.com.vn/the-gioi.html");
+        Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+        scanner.useDelimiter("\\Z");
+        String content = scanner.next();
+        scanner.close();
+        content = content.replaceAll("\\n+", "");
+        Pattern p = Pattern.compile("name_song\">(.?)</a>");
+        Matcher m = p.matcher(content);
+        while (m.find()) {
+            System.out.println(m.group(1));
+        }
+    }catch(MalformedURLException e)
+            e.printStackTrace();
+    {
+        e.printStackTrace();
+    }catch(
+    UnsupportedEncodingException e){
+        e.printStackTrace();
+    }catch
+
+    {
+        (
+                IOException e)
+    }
+    {
+        e.printStackTrace();
+    }
+}
+}
